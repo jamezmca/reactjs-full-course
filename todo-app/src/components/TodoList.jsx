3277c2cea15bb1b5ce3fdbd4e557839c812ed27a
James McArthur <77163783+jamezmca@users.noreply.github.com>
2024-12-27 11:18:05 +1300

    Update TodoList.jsx

--- todo-app/src/components/TodoList.jsx
+++ todo-app/src/components/TodoList.jsx
@@ -15,6 +15,7 @@ export function TodoList(props) {
             {filterTodosList.map((todo, todoIndex) => {
                 const tempTodoIndex = todos.findIndex(val => val.input == todo.input)
                 console.log(tempTodoIndex)
+                // finding an index like this has it's limitations and in later courses we learn a more sophisticated way of doing it (the limitation is that the code might misbehave if you have two todos with the exact same text :) See if you can figure out why!)
                 return (
                     <TodoCard
                         key={todoIndex}
